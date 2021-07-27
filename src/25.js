206e64b22b77bc4737f17f5e9858a1b81acd47ce
santosh <santosh.ece06@gmail.com>
2021-07-27 19:48:25 +0530

    arrow.

---
+++ src/25.js
@@ -0,0 +1,9 @@
+function sum() {}
+let sum1 = () => {};
+
+let myrandom = ({ id = 0, name = "Uddipta Gogoi", email = "", mobile }) => {
+  let list = [];
+  list.push(name);
+
+  console.log(list);
+};
